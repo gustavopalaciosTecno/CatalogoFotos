068e68eed3ddd49d668aa5fbd7abd45150ac3fb2
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2026-06-23 22:47:27 -0300

    Catálogo de fotos

--- script.js
+++ script.js
@@ -119,17 +119,195 @@ const galeriaPorCategorias = {
         { src: 'img/08-CLASIFICAMOS/24.jpeg', descripcion: 'los clasificamos!!!' },
         
     ],
+
+     "JUGAMOS CON JUEGOS GEOMETRICOS!!!": [
+        { src: 'img/09-JUGAMOS_CON/01.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/02.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/03.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/04.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/05.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/06.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/07.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/08.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/09.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/10.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/11.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/12.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+        { src: 'img/09-JUGAMOS_CON/13.jpeg', descripcion: 'Jugamos con Juegos geométricos!!!' },
+    ],
+
+    "MODELAMOS Y SEGUIMOS APRENDIENDO!!!": [
+        { src: 'img/10-MODELAMOS/01.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/02.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/03.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/04.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/05.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/06.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/07.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/08.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/09.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/10.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/11.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/12.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/13.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/14.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/15.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/16.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+        { src: 'img/10-MODELAMOS/17.jpeg', descripcion: 'Modelamos y Seguimos Aprendiendo!!!' },
+    ],
+
+    "LA 'ROBOTITA' NOS VISITA Y CON ELLA SEGUIMOS REFORZANDO CONTENIDOS!!!": [
+        { src: 'img/11-ROBOTITA/01.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/02.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/03.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/04.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/05.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/06.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/07.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/08.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/09.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/10.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/11.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/12.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/13.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/14.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/15.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/16.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/17.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/18.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/19.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/20.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/21.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/22.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/23.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/24.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/25.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/26.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/27.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/28.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/29.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/30.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/31.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/32.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/33.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/34.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/35.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/36.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/37.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/38.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/39.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/40.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/41.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/42.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/43.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/44.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/45.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/46.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/47.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/48.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/49.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/50.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+        { src: 'img/11-ROBOTITA/51.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+         { src: 'img/11-ROBOTITA/52.jpeg', descripcion: 'La Robotita nos visita y con ella seguimos reforzando contenidos' },
+       
+    ],
+
+    "JUGAMOS A FORMAR FIGURAS CON DISTINTOS MATERIALES": [
+        { src: 'img/12-JUGAMOS_FIGURAS/01.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/02.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/03.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/04.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/05.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/06.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/07.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/08.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/09.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/10.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/11.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/12.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/13.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/14.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/15.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/16.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/17.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/18.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/19.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/20.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/21.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/22.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/23.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/24.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/25.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/26.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/27.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/28.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/29.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/30.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/31.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/32.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/33.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/34.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/35.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/36.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/37.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/38.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/39.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+        { src: 'img/12-JUGAMOS_FIGURAS/40.jpeg', descripcion: 'Jugamos a formar figuras con distintos materiales' },
+    ],
+
+    "CONFECCIONAMOS CARTELES": [
+        { src: 'img/13-CONFECCIONAMOS/01.jpeg', descripcion: 'Confecciamos carteles' },
+        { src: 'img/13-CONFECCIONAMOS/02.jpeg', descripcion: 'Confecciamos carteles' },
+    ],
+    
     
 };
 
 const mainElement = document.getElementById('main-gallery');
 
 // Función para renderizar la galería estructurada
+// function renderizarGaleria() {
+//     for (const [titulo, imagenes] of Object.entries(galeriaPorCategorias)) {
+//         // 1. Crear la sección de la categoría
+//         const seccion = document.createElement('section');
+//         seccion.className = 'categoria';
+
+//         // 2. Crear y agregar el título de la categoría
+//         const header = document.createElement('header');
+//         header.className = 'categoria-header';
+//         header.innerHTML = `<h2>${titulo}</h2>`;
+//         seccion.appendChild(header);
+
+//         // 3. Crear el contenedor de las tarjetas (grid)
+//         const grid = document.createElement('div');
+//         grid.className = 'categoria-grid';
+
+//         // 4. Crear y agregar las tarjetas de imagen
+//         imagenes.forEach(imagen => {
+//             const card = document.createElement('article');
+//             card.className = 'card';
+//             card.innerHTML = `
+//                 <img src="${imagen.src}" alt="${imagen.descripcion}" loading="lazy">
+//                 <div class="card-description">${imagen.descripcion}</div>
+//             `;
+//             grid.appendChild(card);
+//         });
+
+//         // 5. Agregar el grid a la sección y la sección al main
+//         seccion.appendChild(grid);
+//         mainElement.appendChild(seccion);
+//     }
+// }
+// REEMPLAZA TU FUNCIÓN ACTUAL POR ESTA VERSIÓN OPTIMIZADA:
 function renderizarGaleria() {
+    let index = 0; // Contador para crear IDs de salto fáciles
+    
     for (const [titulo, imagenes] of Object.entries(galeriaPorCategorias)) {
         // 1. Crear la sección de la categoría
         const seccion = document.createElement('section');
         seccion.className = 'categoria';
+        seccion.id = `cat-${index}`; // Esto genera: id="cat-0", id="cat-1", etc.
+        index++;
 
         // 2. Crear y agregar el título de la categoría
         const header = document.createElement('header');
@@ -157,6 +335,5 @@ function renderizarGaleria() {
         mainElement.appendChild(seccion);
     }
 }
-
 // Ejecutar la función al cargar la página
 renderizarGaleria();
